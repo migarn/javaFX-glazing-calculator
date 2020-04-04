04070f3c31dfc9b58c65b0d6d20069d943525dea
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-04 18:10:30 +0200

    Initial values added to text fields

--- src/com/michalgarnczarski/Controller.java
+++ src/com/michalgarnczarski/Controller.java
@@ -2,15 +2,29 @@ package com.michalgarnczarski;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
     private Label headerLabel;
 
+    @FXML
+    private TextField glassWidthTextField;
+
+    @FXML
+    private TextField glassHeightTextField;
+
+    @FXML
+    private TextField spacerWidthTextField;
+
     public void initialize() {
         headerLabel.setText("Kalkulator służy do obliczania minimalnych grubości szyb." +
                 "\nObliczenia oparte są na normie zakładowej firmy PRESS GLASS SA.");
+
+        glassWidthTextField.setText("0");
+        glassHeightTextField.setText("0");
+        spacerWidthTextField.setText("16");
+
     }
 }
