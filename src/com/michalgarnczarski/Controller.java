9a52d3ed0aaef19452795bd98c5b1171c3992402
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-04 15:08:51 +0200

    styles.css added and padding defined

--- src/com/michalgarnczarski/Controller.java
+++ src/com/michalgarnczarski/Controller.java
@@ -1,4 +1,16 @@
 package com.michalgarnczarski;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    @FXML
+    private Label headerLabel;
+
+    public void initialize() {
+        headerLabel.setText("Kalkulator służy do obliczania minimalnych grubości szyb." +
+                "\nObliczenia oparte są na normie zakłądowej firmy PRESS GLASS SA");
+    }
 }
