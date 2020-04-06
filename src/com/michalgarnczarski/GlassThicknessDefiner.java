18d7a4fb18df7ea36d1cb770f3e661578f96f730
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-06 19:22:24 +0200

    GlassThicknessDefiner completely implemented

--- src/com/michalgarnczarski/GlassThicknessDefiner.java
+++ src/com/michalgarnczarski/GlassThicknessDefiner.java
@@ -40,4 +40,50 @@ public class GlassThicknessDefiner {
             return -1;
         }
     }
+
+    public int defineThicknessFor12() {
+        if (glass.getArea() <= 1.5 && glass.getLongerDimension() <= 1500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 3;
+        } else if (glass.getArea() <= 3.35 && glass.getLongerDimension() <= 2500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 4;
+        } else if (glass.getArea() <= 5 && glass.getLongerDimension() <= 3300) {
+            return 5;
+        } else if (glass.getArea() <= 7 && glass.getLongerDimension() <= 3500) {
+            return 6;
+        } else if (glass.getArea() <= 8.75 && glass.getLongerDimension() <= 3500) {
+            return 8;
+        } else {
+            return -1;
+        }
+    }
+
+    public int defineThicknessFor9() {
+        if (glass.getArea() <= 1.5 && glass.getLongerDimension() <= 1500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 3;
+        } else if (glass.getArea() <= 2.5 && glass.getLongerDimension() <= 2500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 4;
+        } else if (glass.getArea() <= 3.5 && glass.getLongerDimension() <= 3000) {
+            return 5;
+        } else if (glass.getArea() <= 4.5 && glass.getLongerDimension() <= 3000) {
+            return 6;
+        } else if (glass.getArea() <= 6 && glass.getLongerDimension() <= 3000) {
+            return 8;
+        } else {
+            return -1;
+        }
+    }
+
+    public int defineThicknessFor6() {
+        if (glass.getArea() <= 2 && glass.getLongerDimension() <= 200 && glass.getDimensionsRatio() > 1 / 6) {
+            return 4;
+        } else if (glass.getArea() <= 2.5 && glass.getLongerDimension() <= 2500) {
+            return 5;
+        } else if (glass.getArea() <= 3 && glass.getLongerDimension() <= 3000) {
+            return 6;
+        } else if (glass.getArea() <= 4 && glass.getLongerDimension() <= 3000) {
+            return 8;
+        } else {
+            return -1;
+        }
+    }
 }
