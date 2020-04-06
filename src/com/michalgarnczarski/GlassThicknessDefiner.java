0df6aac19d8384c011ac6b8df383929c3f54a75e
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-06 19:14:59 +0200

    defineThicknessFor16 implemented

--- src/com/michalgarnczarski/GlassThicknessDefiner.java
+++ src/com/michalgarnczarski/GlassThicknessDefiner.java
@@ -20,4 +20,24 @@ public class GlassThicknessDefiner {
             return -1;
         }
     }
+
+    public int defineThicknessFor16() {
+        if (glass.getArea() <= 1.5 && glass.getLongerDimension() <= 1500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 3;
+        } else if (glass.getArea() <= 3.35 && glass.getLongerDimension() <= 2500 && glass.getDimensionsRatio() > 1 / 6) {
+            return 4;
+        } else if (glass.getArea() <= 5 && glass.getLongerDimension() <= 3300) {
+            return 5;
+        } else if (glass.getArea() <= 7 && glass.getLongerDimension() <= 3500) {
+            return 6;
+        } else if (glass.getArea() <= 10 && glass.getLongerDimension() <= 5000) {
+            return 8;
+        } else if (glass.getArea() <= 13.5 && glass.getLongerDimension() <= 5000) {
+            return 10;
+        } else if (glass.getArea() <= 13.5 && glass.getLongerDimension() <= 6000) {
+            return 12;
+        } else {
+            return -1;
+        }
+    }
 }
