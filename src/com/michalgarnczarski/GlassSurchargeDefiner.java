208d45b4de5c1d014c83f85f057ad67eb39f4c37
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-07 16:51:34 +0200

    GlassSurchargeDefiner class implemented

---
+++ src/com/michalgarnczarski/GlassSurchargeDefiner.java
@@ -0,0 +1,32 @@
+package com.michalgarnczarski;
+
+public class GlassSurchargeDefiner {
+    private Glass glass;
+
+    public GlassSurchargeDefiner(Glass glass) {
+        this.glass = glass;
+    }
+
+    public int defineSurcharge() {
+        if (glass.getLongerDimension() <= 3000 && glass.getShorterDimension() <= 2400) {
+            return 0;
+        } else if ((glass.getLongerDimension() > 2400 && glass.getLongerDimension() <= 3000 && glass.getShorterDimension() > 2400 && glass.getShorterDimension() <= 2700) ||
+                (glass.getLongerDimension() > 3000 && glass.getLongerDimension() <= 4000 && glass.getShorterDimension() < 2000)) {
+            return 50;
+        } else if ((glass.getLongerDimension() > 3000 && glass.getLongerDimension() <= 4000 && glass.getShorterDimension() > 2000 && glass.getShorterDimension() <= 2400) ||
+                (glass.getLongerDimension() > 4000 && glass.getLongerDimension() <= 5000 && glass.getShorterDimension() < 2000)) {
+            return 75;
+        } else if ((glass.getLongerDimension() > 3000 && glass.getLongerDimension() <= 4000 && glass.getShorterDimension() > 2400 && glass.getShorterDimension() <= 2700) ||
+                (glass.getLongerDimension() > 4000 && glass.getLongerDimension() <= 5000 && glass.getShorterDimension() > 2000 && glass.getShorterDimension() <= 2400) ||
+                (glass.getLongerDimension() > 5000 && glass.getLongerDimension() <= 6000 && glass.getShorterDimension() < 2000)) {
+            return 100;
+        } else if ((glass.getLongerDimension() > 4000 && glass.getLongerDimension() <= 5000 && glass.getShorterDimension() > 2400 && glass.getShorterDimension() <= 2700) ||
+                (glass.getLongerDimension() > 5000 && glass.getLongerDimension() <= 6000 && glass.getShorterDimension() > 2000 && glass.getShorterDimension() <= 2400)) {
+            return 125;
+        } else if (glass.getLongerDimension() > 5000 && glass.getLongerDimension() <= 6000 && glass.getShorterDimension() > 2400 && glass.getShorterDimension() <= 2700) {
+            return 150;
+        } else {
+            return -1;
+        }
+    }
+}
